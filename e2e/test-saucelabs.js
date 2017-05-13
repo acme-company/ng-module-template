63d757cf7916d6de0455b73adb0a712bf7604321
pixelbits <pixelbits100@gmail.com>
2017-05-13 13:44:43 -0600

    done

--- e2e/test-saucelabs.js
+++ e2e/test-saucelabs.js
@@ -1,5 +1,6 @@
 var exec = require('child_process').exec;
+var helpers = require('../helpers.js');
 
 // execute the process
-var karma = exec('karma start karma.conf.js --CI_MODE=saucelabs --BROWSER_KEY=CI_REQUIRED');
+var karma = exec(`karma start ${helpers.root('e2e/karma.conf.js')} --CI_MODE=saucelabs --BROWSER_KEY=CI_REQUIRED`);
 karma.stdout.pipe(process.stdout);
