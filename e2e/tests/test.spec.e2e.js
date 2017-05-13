02ddc95f74b38b8aa2ac59d5e8c46ce123d02d94
pixelbits <pixelbits100@gmail.com>
2017-05-12 18:26:42 -0600

    test

--- e2e/tests/test.spec.e2e.js
+++ e2e/tests/test.spec.e2e.js
@@ -1,46 +1,6 @@
 var username = 'pixelbits-mk'; // process.env.SAUCE_USERNAME;
 var accessKey = "704b59a8-e1b1-4ee2-abd1-38a0acc6376c";//process.env.SAUCE_ACCESS_KEY;
-// var SauceLabs = require("saucelabs");
 
-// var saucelabs = new SauceLabs({
-//     username: username,
-//     password: accessKey
-// });
-
-// var webdriver = require('selenium-webdriver'),
-//         username = username,
-//         accessKey = accessKey,
-//         driver;
-
-
-
-// driver = new webdriver.Builder().
-//       withCapabilities({
-//             'browserName': 'chrome',
-//             'platform': 'Windows XP',
-//             'version': '43.0',
-//             'username': username,
-//             'accessKey': accessKey
-//       }).
-//       usingServer("http://" + username + ":" + accessKey +
-//                   "@ondemand.saucelabs.com:80/wd/hub").
-//       build();
-
-// driver.get("http://saucelabs.com/test/guinea-pig");
-
-// driver.getTitle().then(function (title) {
-//         console.log("title is: " + title);
-// });
-
-
-// driver.quit();
-
-// saucelabs.updateJob(driver.sessionID, {
-//     name: 'this is a test',
-//     passed: false
-// }, function () {
-//     console.log('done');
-//  });
 
 var browser = process.env.BROWSER,
   version = process.env.VERSION,
