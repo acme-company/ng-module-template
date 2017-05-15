58527128aa5bef98dfdb597a7dea5268897bff0f
pixelbits-mk <pixelbits-mk@users.noreply.github.com>
2017-05-15 00:02:43 -0600

    Update launchSaucelabs.js

--- e2e/launchSaucelabs.js
+++ e2e/launchSaucelabs.js
@@ -4,7 +4,7 @@ var helpers = require('../helpers.js');
 
 module.exports = (function() {
     // execute the process
-    var args = process.argv.slice(2, process.argv.length);
+    var args = process.argv.slice(2);
     var csv = args.map(t=>t.trim()).join(',');
  
     var karma = exec(`karma start ${helpers.root('e2e/karma.conf.js')} --CI_MODE=saucelabs --BROWSER_CATEGORY=${csv}`);
