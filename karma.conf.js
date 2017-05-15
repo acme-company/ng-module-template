30b844c6f03eb4f06f5d68a7206793e33826b77c
pixelbits <pixelbits100@gmail.com>
2017-05-15 02:00:38 -0600

    added coverage

--- karma.conf.js
+++ karma.conf.js
@@ -51,18 +51,15 @@ module.exports = function (config) {
     webpack: testWebpackConfig,
 
     coverageReporter: {
-      type: 'in-memory',
-      reporters: [
-        // reporters not supporting the `file` property 
-        { type: 'html', subdir: 'report-html' },
-        { type: 'lcov', subdir: 'report-lcov' }
-      ]
+      type: 'in-memory'
     },
 
     remapCoverageReporter: {
       'text-summary': null,
       json: './coverage/coverage.json',
-      html: './coverage/html'
+      html: './coverage/html',
+      cobertura: './coverage/cobertura.xml',
+      lcovonly: './coverage/lcov.info'
     },
 
     // Webpack please don't spam the console when running in karma!
